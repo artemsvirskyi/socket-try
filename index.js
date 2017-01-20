17c42ed9a1edeed00dabf57533f88b3ae2a6371a
Artem Svirskyi <artem.sirskyi@gmail.com>
2017-01-20 17:57:12 +0200

    first

---
+++ index.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var messages = [];
+var activeSockets = [];
+var i = 0;
+
+io.on('connection', function(socket){
+    var index = ++i;
+
+    activeSockets.push({
+        index: index,
+        socket: socket
+    });
+
+    console.log('connection', index);
+
+    socket.emit('messages', {messages: messages, connection: index});
+
+    socket.on('add', function(message){
+        messages = messages.concat(message);
+
+        activeSockets.forEach(function(socketData){
+            socketData.socket.emit('messages', {messages: messages, connection: index});
+        });
+    });
+
+    socket.on('disconnect', function () {
+        console.log('disconnect', index);
+
+        removeActiveSocketByIndex(activeSockets, index);
+
+        activeSockets.forEach(function(socket){
+            console.log(socket.index);
+        });
+    });
+});
+
+function removeActiveSocketByIndex(sockets, id){
+    var index = getSocketIndexInArray(sockets, id);
+
+    console.log('removing index - ', index);
+
+    sockets.splice(index, 1);
+}
+
+function getSocketIndexInArray(sockets, id){
+    var socketIndexInArray = -1;
+
+    sockets.some(function(socket, socketIndex){
+        var isTheOne = socket.index === id;
+
+        if(isTheOne){
+            socketIndexInArray = socketIndex
+        }
+
+        return isTheOne;
+    });
+
+    return socketIndexInArray;
+}
+
+app.use(express.static('public'));
+
+server.listen(3000);
